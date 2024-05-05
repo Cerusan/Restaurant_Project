0a53847f36240d1196b0a114a7b6ede8260ad404
Vlad Tastenov <vladtastenov@gmail.com>
2024-05-05 13:32:07 +0500

    add starter create logic for Food

--- src/main/java/ru/cerus/restaurantproject/controller/MenuController.java
+++ src/main/java/ru/cerus/restaurantproject/controller/MenuController.java
@@ -3,9 +3,13 @@ package ru.cerus.restaurantproject.controller;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.cerus.restaurantproject.domain.menu.AbstractMenu;
+import ru.cerus.restaurantproject.domain.menu.Category;
 import ru.cerus.restaurantproject.domain.menu.Drink;
 import ru.cerus.restaurantproject.domain.menu.Food;
 import ru.cerus.restaurantproject.service.AbstractMenuService;
@@ -39,6 +43,27 @@ public class MenuController {
         List<Food> foodList = abstractMenuService.getAllFood();
         model.addAttribute("foods", foodList);
         return "menu/food";
+    }
+
+    @GetMapping("/create")
+    public String createMenu() {
+        return "menu/addMenuPage";
+    }
+
+    @GetMapping("/create/addFood")
+    public String createFood(@ModelAttribute("newDish") Food food, Model model) {
+        List<Category> categoryList = categoryService.getAllCategory();
+        return "menu/addFood";
+    }
+
+    @PostMapping("/create/addFood/process_addFood")
+    public String createFoodProcess(@ModelAttribute("newDish") Food food,
+                                    BindingResult bindingResult) {
+        if (bindingResult.hasErrors())
+            return "menu/addFood";
+
+        abstractMenuService.createAbstractMenu(food);
+        return "redirect:/menu/foods";
     }
 
 }
